8f493184523be97fa6bd5de4accd4112d1df76c6
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2015-10-29 15:34:39 -0500

    Created class to handle GPIO pins

--- Garden.py
+++ Garden.py
@@ -1,34 +1,10 @@
 #!/usr/bin/python
 # Test program
-import RPi.GPIO as GPIO
-import time
-import os
-import random
+import PinController
 
-from array import array
-sleepTime = .1
-GPIO.setmode(GPIO.BCM)
+pinController = PinController()
 
-relays= [21,20,22,27,17,18,24,23]
-
-for x in relays:
-	GPIO.setup(x, GPIO.OUT)
-
-
-def pinControl(sleepTime):
-	for y in range(0,5):
-		
-		random.shuffle(relays)
-		for x in relays:
-
-			GPIO.output(x, False)
-			time.sleep(sleepTime)
-			print "Loop " + str(x)
-			GPIO.output(x, True)
-			time.sleep(sleepTime)
-
-
-pinControl(sleepTime)
+pinController.controlPin(21, True)
 
 GPIO.cleanup()
 GPIO.setwarnings(False)
