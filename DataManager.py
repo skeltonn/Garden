8553c49de5a1c6a78affd38372a58b5c3e86aa45
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2016-04-20 08:23:11 -0500

    Resolved compile issues

--- DataManager.py
+++ DataManager.py
@@ -1,8 +1,6 @@
 import pymysql
 
 class DataManager:
-
-	connection = null
 	
 	@staticmethod
 	def getLatestMoisture():
@@ -15,12 +13,12 @@ class DataManager:
                              cursorclass=pymysql.cursors.DictCursor)
                              
 		try:
-    		with connection.cursor() as cursor:
-        		# Read a single record
-        		sql = "SELECT zone1, zone2, zone3, zone4 FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
-        		cursor.execute(sql)
-        		result = cursor.fetchone()
-        		print(result)
+	    		with connection.cursor() as cursor:
+        			# Read a single record
+        			sql = "SELECT zone1, zone2, zone3, zone4 FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
+        			cursor.execute(sql)
+        			result = cursor.fetchone()
+        			print(result)
         		
 		finally:
-    		connection.close()
+    			connection.close()
