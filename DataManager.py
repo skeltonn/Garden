5e3d3e1ad261438032e4decaa54ee76fe26a2330
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2016-04-20 08:44:18 -0500

    Built accessor methods for values regarding watering

--- DataManager.py
+++ DataManager.py
@@ -1,24 +1,72 @@
 import pymysql
 
 class DataManager:
-	
-	@staticmethod
-	def getLatestMoisture():
-		
-		connection = pymysql.connect(host='localhost',
+
+	connection = pymysql.connect(host='localhost',
                              user='pi',
                              password='GardenPi',
                              db='Garden',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-                             
+	
+	@staticmethod
+	def getLatestMoisture():
+	
 		try:
 	    		with connection.cursor() as cursor:
         			# Read a single record
         			sql = "SELECT zone1, zone2, zone3, zone4 FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
         			cursor.execute(sql)
         			result = cursor.fetchone()
-        			print(result)
+        			return [result["zone1"], result["zone2"], result["zone3"], result["zone4"]]
         		
 		finally:
     			connection.close()
+
+	@staticmethod
+	def getLatestRainfall():
+	
+		try:
+	    		with connection.cursor() as cursor:
+        			# Read a single record
+        			sql = "SELECT rain FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
+        			cursor.execute(sql)
+        			result = cursor.fetchone()
+        			return result["rain"]
+        		
+		finally:
+    			connection.close()
+    			
+    @staticmethod
+	def getPredictedRainfall():
+	
+		try:
+	    		with connection.cursor() as cursor:
+        			# Read a single record
+        			sql = "SELECT pchance, prain FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
+        			cursor.execute(sql)
+        			result = cursor.fetchone()
+        			return [result["pchance"], result["prain"]]
+        		
+		finally:
+    			connection.close()
+    			
+    @staticmethod
+	def getPreviousWateringTimes():
+	
+		try:
+	    		with connection.cursor() as cursor:
+        			# Read a single record
+        			sql = "SELECT water1, water2, water3, water4 FROM observed where record BETWEEN (NOW() - INTERVAL 2 DAY) AND NOW() order by record desc"
+        			cursor.execute(sql)
+        			result = cursor.fetchall()
+        			for row in result:
+        				if row[0] > 0 or row[1] > 0 or row[2] > 0 or row[3] > 0 or row[4] > 0;
+        					return row
+        			return [0,0,0,0]
+        		
+		finally:
+    			connection.close()
+    			
+    			
+    			
