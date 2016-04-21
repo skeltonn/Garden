4f05a9fc7bac75bad692deefca17bba6c69cef7e
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2016-04-21 11:01:29 -0500

    fixed formatting, globalized result

--- DataManager.py
+++ DataManager.py
@@ -3,122 +3,103 @@ import pymysql
 class DataManager:
 
 	connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='',
-                             db='Garden',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+    	user='root',
+    	password='',
+        db='Garden',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor)
+                             
+    result = ""
 	
 	@staticmethod
 	def getLatestMoisture():
 	
-		result = ""
 		try:
-	    		with connection.cursor() as cursor:
-	    			nonlocal result
-        			# Read a single record
-        			sql = "SELECT zone1, zone2, zone3, zone4 FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
-        			cursor.execute(sql)
-        			result = cursor.fetchone()
+	    	with connection.cursor() as cursor:
+	    		global result
+    			sql = "SELECT zone1, zone2, zone3, zone4 FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
+    			cursor.execute(sql)
+       			result = cursor.fetchone()
         			
 		finally:
-				nonlocal result
-    			connection.close()
-    			return [result["zone1"], result["zone2"], result["zone3"], result["zone4"]]
+			global result
+    		connection.close()
+    		return [result["zone1"], result["zone2"], result["zone3"], result["zone4"]]
 
 	@staticmethod
 	def getLatestRainfall():
 	
-		result = ""
 		try:
-	    		with connection.cursor() as cursor:
-	    			nonlocal result
-        			# Read a single record
-        			sql = "SELECT rain FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
-        			cursor.execute(sql)
-        			result = cursor.fetchone()
+	    	with connection.cursor() as cursor:
+	    		global result
+    			sql = "SELECT rain FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
+    			cursor.execute(sql)
+    			result = cursor.fetchone()
         			
 		finally:
-				nonlocal result
-    			connection.close()
-    			return result["rain"]
+			global result
+    		connection.close()
+    		return result["rain"]
     			
     @staticmethod
 	def getPredictedRainfall():
 	
-		result = ""
 		try:
-	    		with connection.cursor() as cursor:
-	    			nonlocal result
-        			# Read a single record
-        			sql = "SELECT pchance, prain FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
-        			cursor.execute(sql)
-        			result = cursor.fetchone()
+	    	with connection.cursor() as cursor:
+	    		global result
+        		sql = "SELECT pchance, prain FROM observed where record BETWEEN (NOW() - INTERVAL 1 DAY) AND NOW() order by record desc limit 1"
+        		cursor.execute(sql)
+        		result = cursor.fetchone()
         		
 		finally:
-				nonlocal result
-    			connection.close()
-    			return [result["pchance"], result["prain"]]
+			global result
+    		connection.close()
+    		return [result["pchance"], result["prain"]]
     			
     @staticmethod
 	def getPreviousWateringTimes():
 	
-		result = ""
 		try:
-	    		with connection.cursor() as cursor:
-	    			nonlocal result
-        			# Read a single record
-        			sql = "SELECT record, water1, water2, water3, water4 FROM observed where record BETWEEN (NOW() - INTERVAL 2 DAY) AND NOW() order by record desc"
-        			cursor.execute(sql)
-        			result = cursor.fetchall()
+	    	with connection.cursor() as cursor:
+	    		global result
+        		sql = "SELECT record, water1, water2, water3, water4 FROM observed where record BETWEEN (NOW() - INTERVAL 2 DAY) AND NOW() order by record desc"
+        		cursor.execute(sql)
+        		result = cursor.fetchall()
         		
 		finally:
-				nonlocal result
-    			connection.close()
-    			for row in result:
-        				if row[1] > 0 or row[2] > 0 or row[3] > 0 or row[4] > 0;
-        					return row
-        			return ["2000-01-01 01:00:00", 0, 0, 0, 0]
+			global result
+    		connection.close()
+    		for row in result:
+        			if row[1] > 0 or row[2] > 0 or row[3] > 0 or row[4] > 0;
+        				return row
+        	return ["2000-01-01 01:00:00", 0, 0, 0, 0]
     			
     @staticmethod
 	def getSprinklerWaterRate():
 	
-		result = ""
 		try:
-	    		with connection.cursor() as cursor:
-	    			nonlocal result
-        			# Read a single record
-        			sql = "SELECT gpm FROM calibration"
-        			cursor.execute(sql)
-        			result = cursor.fetchall()
+	    	with connection.cursor() as cursor:
+	    		global result
+        		sql = "SELECT gpm FROM calibration"
+        		cursor.execute(sql)
+        		result = cursor.fetchall()
         		
 		finally:
-				nonlocal result
-    			connection.close()
-    			return [result[0], result[1], result[2], result[3]]
+			global result
+    		connection.close()
+    		return [result[0], result[1], result[2], result[3]]
     			
     @staticmethod
 	def getTargetCapacity():
-
-		result = ""
+	
 		try:
-	    		with connection.cursor() as cursor:
-	    			nonlocal result
-        			# Read a single record
-        			sql = "SELECT capcity FROM calibration"
-        			cursor.execute(sql)
-        			result = cursor.fetchall()
-        		
+	    	with connection.cursor() as cursor:
+	    		global result
+        		sql = "SELECT capcity FROM calibration"
+        		cursor.execute(sql)
+        		result = cursor.fetchall()
+        	
 		finally:
-				nonlocal result
-    			connection.close()
-    			return result
-    			
-    			
-    			
-    			
-    			
-    			
-    			
-    			
-    			
+			global result
+    		connection.close()
+    		return result
