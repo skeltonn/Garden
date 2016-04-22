049fe81ae37b70aa10777085a911754c397b174c
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2016-04-22 08:09:14 -0500

    Proper spacing

--- WateringCalculator.py
+++ WateringCalculator.py
@@ -1,3 +1,4 @@
+import pymysql
 from DataManager import DataManager
 
 class WateringCalculator:
@@ -8,29 +9,29 @@ class WateringCalculator:
 		result = []
 		
 		recentWateringGallons = DataManager.getPreviousWateringAmounts(pymysql.connect(host='localhost',
-    		user='root',
-    		password='',
-        	db='Garden',
-        	charset='utf8mb4',
-        	cursorclass=pymysql.cursors.DictCursor))
+	    		user='root',
+    			password='',
+        		db='Garden',
+       		 	charset='utf8mb4',
+        		cursorclass=pymysql.cursors.DictCursor))
 		sectorTargets = DataManager.getTargetCapacity(pymysql.connect(host='localhost',
-    		user='root',
-    		password='',
-        	db='Garden',
-        	charset='utf8mb4',
-        	cursorclass=pymysql.cursors.DictCursor))
-		previousRain = DataManager.getLatestRain(pymysql.connect(host='localhost',
-    		user='root',
-    		password='',
-        	db='Garden',
-        	charset='utf8mb4',
-    	    cursorclass=pymysql.cursors.DictCursor))
+    			user='root',
+    			password='',
+        		db='Garden',
+        		charset='utf8mb4',
+        		cursorclass=pymysql.cursors.DictCursor))
+		previousRain = DataManager.getLatestRainfall(pymysql.connect(host='localhost',
+    			user='root',
+    			password='',
+        		db='Garden',
+        		charset='utf8mb4',
+    			cursorclass=pymysql.cursors.DictCursor))
 		currentMoistures = DataManager.getLatestMoisture(pymysql.connect(host='localhost',
-    		user='root',
-    		password='',
-        	db='Garden',
-        	charset='utf8mb4',
-        	cursorclass=pymysql.cursors.DictCursor))
+    			user='root',
+    			password='',
+        		db='Garden',
+        		charset='utf8mb4',
+        		cursorclass=pymysql.cursors.DictCursor))
 		
 		for x in range(0, 4):
 			if currentMoistures[x] > sectorTargets[x]:
@@ -39,5 +40,6 @@ class WateringCalculator:
 				result.insert(x, 0)
 			else:
 				result.insert(x, 1)
+			print((previousRain * 560) + recentWateringGallons[x + 1])
 		
 		return result
